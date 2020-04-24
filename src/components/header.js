7b434a466e4dc9a3531faef04b05c0457fe14585
Hugo <apple@Apples-MBP.lan>
2020-04-24 13:31:01 +0800

    added section 2

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,42 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './Headers.css';
 
-const Header = ({ siteTitle }) => (
-  <div className='Header'>
-    <div className='HeaderGroup'>
-      <Link to='/'><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
-      <Link to='/courses'>Courses</Link>
-      <Link to='/downloads'>Downloads</Link>
-      <Link to='/workshops'>Workshops</Link>
-      <Link to='/buy'><button>Buy</button></Link>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScrolled)
+  }
+  
+handleScrolled = (event) => {
+  const scrollTop = window.pageYOffset
+
+  if (scrollTop > 50) {
+    this.setState({ handleScrolled: true })
+  } else {
+    this.setState({ handleScrolled: false })
+  }
+}
+
+  render() {
+    return (
+      <div className={this.state.handleScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className='HeaderGroup'>
+          <Link to='/'><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
+          <Link to='/courses'>Courses</Link>
+          <Link to='/downloads'>Downloads</Link>
+          <Link to='/workshops'>Workshops</Link>
+          <Link to='/buy'><button>Buy</button></Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
