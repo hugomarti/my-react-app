3ca4baed6cb73a35735992977c71f4d6f5679972
Hugo <apple@Apples-MBP.lan>
2020-04-24 19:34:53 +0800

    fixed issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,14 +4,5 @@ module.exports = {
     description: 'Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.',
     keywords: 'react course, react for designers, ios development, sktech app, swift app course, arkit 2, after effects, create sktech plugin'
   },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-source-contentful',
-      options: {
-        spaceId: 'bt4932zd149v',
-        accessToken: 'CZdksDJUVzkGRp9GnvufYuSpzlX_kxfQ6VaRPdRSAuo'
-      }
-    }
-  ],
+  plugins: ['gatsby-plugin-react-helmet'],
 }
